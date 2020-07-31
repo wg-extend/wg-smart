478cffc6ca96d3374e6e8683e7f86af78c1318bc
wangliheng <springioc@yeah.net>
2020-07-31 14:39:36 +0800

    fix test

--- src/test/java/com/wg/smart/SmartApplicationTests.java
+++ src/test/java/com/wg/smart/SmartApplicationTests.java
@@ -1,13 +1,8 @@
 package com.wg.smart;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
